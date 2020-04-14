6a93fe21c2872503374f5c997106725b3b6efdcf
Jacob Lehenbauer <jake@lehenbauer.us>
2020-04-14 16:13:01 -0500

    Added Assignment 1 files

---
+++ Assignment1/CheckerboardKarel.py
@@ -0,0 +1,19 @@
+from karel.stanfordkarel import *
+
+"""
+File: CheckerboardKarel.py
+----------------------------
+When you finish writing it, CheckerboardKarel should draw
+a checkerboard using beepers, as described in Assignment 1. 
+You should make sure that your program works for all of the 
+sample worlds supplied in the starter folder.
+"""
+
+
+def main():
+    move()
+    turn_left()
+
+
+if __name__ == "__main__":
+    run_karel_program()
