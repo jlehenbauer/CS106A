586342ee9c14e21d45de05701e1d18c368035bc3
Jacob Lehenbauer <jake@lehenbauer.us>
2020-04-22 13:44:44 -0500

    Added AI computer player to Nimm. Will always win :)

--- Assignment2/nimm.py
+++ Assignment2/nimm.py
@@ -3,17 +3,32 @@ def main():
     """
     Hosts a game of Nimm to be played by two players.
     """
+    one_player_mode = False
     stones = 20
     player_turn = 1
+
+    # Ask the user if they wish to play one-player mode.
+    choice = input("Would you like to play against me, the computer? (y for yes) ")
+    if choice.lower() == 'y':
+        one_player_mode = True
+        print("You go first!")
 
     # Play until no stones remain
     while stones > 0:
         # Inform the players of how many stones are left to be taken
         print("There are " + str(stones) + " left.")
 
-        # Depending on who's turn it is, ask them for their move,
-        # make sure it's valid, then execute their move
-        take = int(input("Player " + str(player_turn) + " would you like to remove 1 or 2 stones? "))
+        # If the computer player is enabled and it's currently
+        # the computer's turn, make their move.
+        if one_player_mode and player_turn == 2:
+            take = take_computer_player_turn(stones)
+
+        # If it's a 2 player game or player 1's turn,
+        # ask them for their move.
+        else:
+            take = int(input("Player " + str(player_turn) + " would you like to remove 1 or 2 stones? "))
+
+        # Check that the last move was valid
         if check_for_valid_entry(take):
             stones -= take
             if player_turn == 1:
@@ -39,6 +54,35 @@ def check_for_valid_entry(take):
     if take == 1 or take == 2:
         return True
     return False
+
+
+def take_computer_player_turn(stones):
+    """
+    Computer player AI to decide how many stones to remove.
+    Will always win :)
+    :param stones: Number of stones remaining at start of turn
+    :return: Number of stones to take
+    """
+    # Ending cases: if stones < 4, choose
+    # 1 or 2 appropriately to leave player 1
+    # with 1 stone.
+    if stones == 3:
+        print("I'm removing 2 stones.")
+        return 2
+    elif stones == 2:
+        print("I'm removing 1 stone.")
+        return 1
+
+    # Until we reach the ending cases, remove
+    # stones to keep the remaining number even,
+    # so player 1 is forced to choose at 4 stones
+    # remaining.
+    elif stones % 2 == 0:
+        print("I'm removing 2 stones.")
+        return 2
+    else:
+        print("I'm removing 1 stone.")
+        return 1
 
 
 if __name__ == "__main__":
