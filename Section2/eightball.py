c191b9f16acd3f4c9b9a798269d35c96f2279c17
Jacob Lehenbauer <jake@lehenbauer.us>
2020-04-24 09:44:56 -0500

    Added solutions to Section2 problems Eightball and Moonweight

--- Section2/eightball.py
+++ Section2/eightball.py
@@ -4,10 +4,35 @@ let the user type a yes or no question and pick a random answer from a
 set of 6 predetermined answers.
 """
 import random
+MAX_RESPONSES = 6
+
 
 def main():
-    # TODO
-    pass
+    user_question = None
+
+    while user_question != "":
+        user_question = input("Ask a yes or no question: ")
+
+        response = random.randint(1, MAX_RESPONSES)
+        response1 = "As I see it..."
+        response2 = "Ask again later"
+        response3 = "Better not tell you now."
+        response4 = "Cannot predict now."
+        response5 = "Concentrate and ask again"
+        response6 = "Yes!"
+
+        if response == 1:
+            print(response1)
+        if response == 2:
+            print(response2)
+        if response == 3:
+            print(response3)
+        if response == 4:
+            print(response4)
+        if response == 5:
+            print(response5)
+        if response == 6:
+            print(response6)
 
 
 if __name__ == '__main__':
--- Section2/moonweight.py
+++ Section2/moonweight.py
@@ -3,10 +3,15 @@ Write a program to prompt the user for a weight on earth and print
 the equivalent weight on the moon.
 """
 
-
 def main():
-    # TODO
-    pass
+    # Take earth weight from the user
+    earth_weight = float(input("Enter weight on earth: "))
+
+    # Convert to moon weight by multiplying by 0.165
+    moon_weight = 0.165 * earth_weight
+
+    # Give back to user
+    print("The weight on the moon would be " + str(moon_weight))
 
 
 if __name__ == '__main__':
