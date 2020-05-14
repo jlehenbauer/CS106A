784b683c8636fe1cf94833f774e0d24f52e9819c
Jacob Lehenbauer <jake@lehenbauer.us>
2020-05-14 10:39:38 -0500

    Added solution to warhol_filter, but using concepts not used in CIP course yet (i.e. lists)

--- Assignment3/warhol_filter.py
+++ Assignment3/warhol_filter.py
@@ -10,26 +10,55 @@ PATCH_SIZE = 222
 WIDTH = N_COLS * PATCH_SIZE
 HEIGHT = N_ROWS * PATCH_SIZE
 PATCH_NAME = 'images/simba-sq.jpg'
+MODS = [
+    [1.5, 0, 1.5],
+    [.7, 1.2, 1.9],
+    [1.2, .4, 0],
+    [1, 1, 1],
+    [0, 1.2, 1.2],
+    [2, 2, 0]
+]
 
 def main():
     final_image = SimpleImage.blank(WIDTH, HEIGHT)
-    # TODO: your code here.
-    # This is an example which should generate a pinkish patch
-    patch = make_recolored_patch(1.5, 0, 1.5)
+
+    for mod in MODS:
+        patch = make_recolored_patch(mod[0], mod[1], mod[2])
+        final_image = attach_patch(patch, final_image, MODS.index(mod))
+
     final_image.show()
+
+
+def attach_patch(patch, image, num):
+    x_pos = (num % N_COLS) * PATCH_SIZE
+    y_pos = (num // N_COLS) * PATCH_SIZE
+
+    for x in range(PATCH_SIZE):
+        for y in range(PATCH_SIZE):
+            px = patch.get_pixel(x, y)
+            image.set_pixel(x + x_pos, y + y_pos, px)
+
+    return image
+
 
 def make_recolored_patch(red_scale, green_scale, blue_scale):
     '''
-    Implement this function to make a patch for the Warhol Filter. It
-    loads the patch image and recolors it.
-    :param red_scale: A number to multiply each pixels' red component by
-    :param green_scale: A number to multiply each pixels' green component by
-    :param blue_scale: A number to multiply each pixels' blue component by
-    :return: the newly generated patch
+    Implement this function to make a patch for the Warhol Filter.
+    It loads the patch image and recolors it.
+    :param red_scale: A number to multiply each pixel's red component by
+    :param green_scale: A number to multiply each pixel's green component by
+    :param blue_scale: A number to multiply each pixel's blue component by
+    Returns the newly generated patch.
     '''
     patch = SimpleImage(PATCH_NAME)
-    # TODO: your code here.
+
+    for px in patch:
+        px.red *= red_scale
+        px.green *= green_scale
+        px.blue *= blue_scale
+
     return patch
+
 
 if __name__ == '__main__':
     main()
